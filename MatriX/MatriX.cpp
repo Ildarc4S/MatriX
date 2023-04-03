8a5402e3bfd9f5373d064496a8aeebd179ffa6b8
Ildar Stepanov <ildarcas@outlook.com>
2023-04-04 00:41:28 +0300

    uraa

--- MatriX/MatriX.cpp
+++ MatriX/MatriX.cpp
@@ -14,7 +14,7 @@ T** Allocator<T>::allocate(size_t rows, size_t cols)
 {
     T** ptr = new T * [rows];
     try {
-        for (size_t i = 0; i < cols; i++)
+        for (size_t i = 0; i < rows; i++)
         {
             ptr[i] = new T[cols];
         }
@@ -22,6 +22,7 @@ T** Allocator<T>::allocate(size_t rows, size_t cols)
     catch (...)
     {
         Allocator::deallocate(ptr, rows);
+        std::cerr << "dfdf";
         throw std::out_of_range("Index out of range");
     }
 
@@ -36,6 +37,7 @@ void Allocator<T>::deallocate(T** ptr, size_t rows)
         delete[] ptr[i];
     }
     delete[] ptr;
+    ptr = nullptr;
 }
 
 
@@ -47,6 +49,7 @@ public:
     virtual size_t getCols() = 0;
     virtual T operator()(size_t rows, size_t cols) = 0;
     //virtual const T& operator()(size_t rows, size_t cols) const = 0;
+    //virtual void resize() = 0;
 
 };
 
@@ -54,16 +57,32 @@ template <typename T>
 class Matrix: public AbstractMatrix<T>
 {
 public:
-    Matrix();
-    Matrix(size_t rows, size_t cols);
-    Matrix(T** arr, size_t cols,size_t rows);
-    //Matrix(const Matrix<T>& other);
+    Matrix(size_t rows = 1, size_t cols = 1, const T value = 1);
+    Matrix(size_t rows, size_t cols, const T** arr);
+    Matrix(const Matrix<T>& other);
+    ~Matrix();
 
 
     size_t getRows() override;
     size_t getCols() override;
     T operator()(size_t rows, size_t cols) override;
-    //const T& operator()(size_t rows, size_t cols) const override;
+    Matrix<T>& operator=(Matrix<T>& other);
+    Matrix<T>& operator+(Matrix<T>& other);
+    Matrix<T>& operator-(Matrix<T>& other);
+    Matrix<T>& operator*(Matrix<T>& other);
+
+    friend std::ostream& operator<<(std::ostream& os, Matrix<T>& matrix)
+    {
+        for (size_t i = 0; i < matrix.rows_; i++)
+        {
+            for (size_t j = 0; j < matrix.cols_; j++)
+            {
+                os << matrix(i,j) << " ";
+            }
+            os << std::endl;
+        }
+        return os;
+    }
 
 private:
     size_t rows_;
@@ -71,27 +90,54 @@ private:
     T** data_;
 };
 
-template <typename T>
-Matrix<T>::Matrix()
-{
-    rows_ = 0;
-    cols_ = 0;
-    data_ = nullptr;
-}
 
 template <typename T>
-Matrix<T>::Matrix(size_t rows, size_t cols):rows_(rows), cols_(cols), data_(Allocator<T>::allocate(rows_,cols_)){}
-
-template <typename T>
-Matrix<T>::Matrix(T** arr, size_t cols, size_t rows):rows_(rows), cols_(cols), data_(Allocator<T>::allocate(rows_,cols_))
+Matrix<T>::Matrix(size_t rows, size_t cols,const T value):rows_(rows), cols_(cols), data_(Allocator<T>::allocate(rows_,cols_))
 {
     for (size_t i = 0; i < rows_; i++)
     {
-        for (size_t j = 0; j < cols; j++)
+        for (size_t j = 0; j < cols_; j++)
+        {
+            data_[i][j] = value;
+        }
+    }
+}
+
+template <typename T>
+Matrix<T>::Matrix(size_t rows, size_t cols, const T** arr):rows_(rows), cols_(cols), data_(Allocator<T>::allocate(rows_,cols_))
+{
+
+    if (arr == nullptr)
+    {
+        std::cerr << "Array pointer is null" << std::endl;
+        throw std::invalid_argument("Array pointer is null");
+    }
+    for (size_t i = 0; i < rows_; i++)
+    {
+        for (size_t j = 0; j < cols_; j++)
         {
             data_[i][j] = arr[i][j];
         }
     }
+}
+
+template <typename T>
+Matrix<T>::Matrix(const Matrix<T>& other): rows_(other.rows_), cols_(other.cols_),data_(Allocator<T>::allocate(rows_,cols_))
+{
+    for (size_t i = 0; i < rows_; i++)
+    {
+        for (size_t j = 0; j < cols_; j++)
+        {
+            data_[i][j] = other.data_[i][j];
+        }
+    }
+}
+
+template <typename T>
+Matrix<T>::~Matrix()
+{
+    Allocator<T>::deallocate(data_, rows_);
+    std::cout << "Memory if free" << std::endl;
 }
 
 template <typename T>
@@ -111,12 +157,112 @@ T Matrix<T>::operator()(size_t rows, size_t cols)
     return data_[rows][cols];
 }
 
+template <typename T>
+Matrix<T>& Matrix<T>::operator=(Matrix<T>& other)
+{
+    if (this != &other) {
+        Allocator<T>::deallocate(data_, rows_);
+
+        rows_ = other.rows_;
+        cols_ = other.cols_;
+
+        data_ = Allocator<T>::allocate(rows_, cols_);
+        for (size_t i = 0; i < rows_; i++) 
+        {
+            for (size_t j = 0; j < cols_; j++) 
+            {
+                data_[i][j] = other.data_[i][j];
+            }
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator+(Matrix<T>& other)
+{
+    if(rows_ != other.rows_ || cols_ != other.cols_) 
+    {
+        std::cerr << "Matrices have different dimensions" << std::endl;
+        throw std::invalid_argument("Matrices have different dimensions");
+    }
+
+    Matrix<T>* result = new Matrix<T>(rows_, cols_);
+    for (size_t i = 0; i < rows_; i++) 
+    {
+        for (size_t j = 0; j < cols_; j++) 
+        {
+            result->data_[i][j] = data_[i][j] + other(i, j);
+        }
+    }
+    return *result;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator-(Matrix<T>& other)
+{
+    if (rows_ != other.rows_ || cols_ != other.cols_)
+    {
+        std::cerr << "Matrices have different dimensions" << std::endl;
+        throw std::invalid_argument("Matrices have different dimensions");
+    }
+
+    Matrix<T>* result = new Matrix<T>(rows_, cols_);
+    for (size_t i = 0; i < rows_; i++)
+    {
+        for (size_t j = 0; j < cols_; j++)
+        {
+            result->data_[i][j] = data_[i][j] - other(i, j);
+        }
+    }
+    return *result;
+}
+
+template <typename T>
+Matrix<T>& Matrix<T>::operator*(Matrix<T>& other)
+{
+    if (cols_ != other.rows_) {
+        throw std::invalid_argument("Matrices are not compatible for multiplication");
+    }
+    Matrix<T>* result = new Matrix<T>(rows_, other.cols_,0);
+
+    for (int i = 0; i < rows_; i++) 
+    {
+        for (int j = 0; j < other.cols_; j++) 
+        {
+            for (int k = 0; k < cols_; k++) 
+            {
+                result->data_[i][j] += data_[i][k] * other(k, j);
+            }
+        }
+    }
+
+    return *result;
+}
+
 
 int main()
 {
-    Matrix<int> A(1, 2);
-    Matrix<float> B(3, 3);
+    int N = 3;
+    int** arr = new int* [N];
+    for (size_t i = 0; i < N; i++)
+    {
+        arr[i] = new int[N];
+        for (size_t j = 0; j < N; j++)
+        {
+            arr[i][j] = 45;
+        }
+    }
 
-
+    Matrix<int> B(3,3,5);
+    Matrix<int> D(3,3);
+    Matrix<int> C(B);
+    B = D;
+    B = D - C;
+    B = D * C;
+    std::cout << D;
+    std::cout << C;
+    std::cout << B;
+    
     return 0;
 }
